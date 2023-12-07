afad78215d68c78e20549b90cae2e709c7a9e6aa
James <jamesflorez11@gmail.com>
2023-12-07 15:53:21 -0800

    work on home, add css

--- src/weather/home/index.js
+++ src/weather/home/index.js
@@ -1,7 +1,23 @@
+import "./index.css";
+import WeatherCard from "../card/weatherCard";
+
 function Home () {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="main-content">
+            <div>
+                <h1>Home</h1>
+                <hr/>
+                <h2>Weather Forecasts</h2>
+                <hr/>
+            </div>
+            <div className="list-group">
+                <WeatherCard/>
+            </div>
+
+            <div>
+                <h2>Weather Channels</h2>
+                <hr/>
+            </div>
         </div>
     )
 }
