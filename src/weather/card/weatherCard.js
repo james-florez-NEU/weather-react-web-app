f869f801729931b0f38c22db025b36a212ccd512
James <jamesflorez11@gmail.com>
2023-12-07 18:03:17 -0800

    Remove details from weather card

--- src/weather/card/weatherCard.js
+++ src/weather/card/weatherCard.js
@@ -29,19 +29,6 @@ const WeatherCard = () => {
                             <h3 className="card-title">{weatherData.location.name}</h3>
                             <p className="card-text">Temperature: {weatherData.current.temp_f}°F</p>
                             <p className="card-text">Condition: {weatherData.current.condition.text}</p>
-                            <ul className="list-group list-group-flush">
-                                <li className={"list-group-item"}>Feels Like: {weatherData.current.feelslike_f}°F</li>
-                                <li className={"list-group-item"}>Precipitation: {weatherData.current.precip_in} in</li>
-                                <li className="list-group-item">Humidity: {weatherData.current.humidity}%</li>
-                                <li className={"list-group-item"}>Pressure: {weatherData.current.pressure_in} in</li>
-                                <li className={"list-group-item"}>UV Index: {weatherData.current.uv}</li>
-                                <li className={"list-group-item"}>Visibility: {weatherData.current.vis_miles} miles</li>
-                                <li className={"list-group-item"}>Cloud Cover: {weatherData.current.cloud} %</li>
-                                <li className="list-group-item">Wind Speed: {weatherData.current.wind_kph} km/h</li>
-                                <li className={"list-group-item"}>Wind Gust: {weatherData.current.gust_kph} km/h</li>
-                                <li className={"list-group-item"}>Wind Direction: {weatherData.current.wind_dir}</li>
-                                <li className={"list-group-item"}>Wind Degree: {weatherData.current.wind_degree}°</li>
-                            </ul>
                         </div>
                     </div>
                 </Link>
