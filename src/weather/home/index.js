844d687fa88960a621fc7d1250787c449cc51390
James <jamesflorez11@gmail.com>
2023-12-08 13:13:45 -0800

    Integrate channels into home page

--- src/weather/home/index.js
+++ src/weather/home/index.js
@@ -6,6 +6,7 @@ import * as client from "../client/client";
 function Home () {
     const [account, setAccount] = useState(null);
     const [favorites, setFavorites] = useState([]);
+    const [channels, setChannels] = useState([]);
     const majorCities = [2801268, 2593241, 2618724, 4059793, 803267, 3332210, 136022];
 
     const fetchAccount = async () => {
@@ -22,25 +23,24 @@ function Home () {
         }
     };
 
+    const fetchChannels = async () => {
+        try {
+            const foundChannels = await client.getChannels();
+            setChannels(foundChannels);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     useEffect(() => {
         fetchAccount();
+        fetchChannels();
     }, []);
 
     return (
         <div className="main-content">
-            <div>
-                <h1>Home</h1>
-                <hr/>
-                <h2>Major Cities</h2>
-                <hr/>
-            </div>
-            <div className="list-group">
-                {majorCities.map((city, cityIndex) => (
-                    <WeatherCard
-                        key={cityIndex}
-                        id={city} />
-                ))}
-            </div>
+            <h1>Home</h1>
+            <hr/>
 
             {favorites && (
                 <div>
@@ -55,6 +55,34 @@ function Home () {
                     </div>
                 </div>
             )}
+
+            <h2>Major Cities</h2>
+            <hr/>
+            <div className="list-group">
+                {majorCities.map((city, cityIndex) => (
+                    <WeatherCard
+                        key={cityIndex}
+                        id={city} />
+                ))}
+            </div>
+            <hr/>
+            <h2>Channels</h2>
+            <hr/>
+            {channels && (
+                <div className="list-group">
+                    {channels.map((channel, channelIndex) => (
+                        <div className="list-group-item" key={channelIndex}>
+                            <h3>{channel.name}</h3>
+                            <p>Description: {channel.description}</p>
+                            <p>City: {channel.city}</p>
+                            <p>Region: {channel.region}</p>
+                            <p>Country: {channel.country}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+
+
 
         </div>
     )
