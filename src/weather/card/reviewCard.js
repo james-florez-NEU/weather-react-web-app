f09072aba2c4137d640fb9aa5d7aeb526985e90b
James <jamesflorez11@gmail.com>
2023-12-09 15:19:32 -0800

    Make ReviewCard a link

--- src/weather/card/reviewCard.js
+++ src/weather/card/reviewCard.js
@@ -1,15 +1,20 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
 const ReviewCard = ({ review, channels, users }) => {
     return (
-        <div className="card">
-            <div className="card-body">
-                <p>Review of Weather Channel: {channels.find(channel => channel._id === review.channel_id)?.name || 'Channel Not Found'}</p>
-                <p>Rating: {review.rating}/10</p>
-                <p>Review: {review.message}</p>
-                <p>Date: {review.date}</p>
-                <p>By: {users.find(user => user._id === review.user_id)?.username || 'User Not Found'}</p>
+        <Link to = {`../reviews/${review._id}`}>
+            <div className="card">
+                <div className="card-body">
+                    <p>Review of Weather Channel: {channels.find(channel => channel._id === review.channel_id)?.name || 'Channel Not Found'}</p>
+                    <p>Rating: {review.rating}/10</p>
+                    <p>Review: {review.message}</p>
+                    <p>Date: {review.date}</p>
+                    <p>By: {users.find(user => user._id === review.user_id)?.username || 'User Not Found'}</p>
 
+                </div>
             </div>
-        </div>
+        </Link>
     );
 };
 export default ReviewCard;
