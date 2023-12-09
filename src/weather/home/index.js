ceb2d6d0e6a0ba6f0966ff5d3c46fb9d70953ad5
James <jamesflorez11@gmail.com>
2023-12-08 20:35:19 -0800

    Make review out of 10

--- src/weather/home/index.js
+++ src/weather/home/index.js
@@ -138,7 +138,7 @@ function Home () {
                         {newestReviews.map((review, reviewIndex) => (
                             <div className="list-group-item" key={reviewIndex}>
                                 <p>Weather Channel: {channels.find(channel => channel._id === review.channel_id)?.name || 'Channel Not Found'}</p>
-                                <p>Rating: {review.rating}</p>
+                                <p>Rating: {review.rating}/10</p>
                                 <p>Review: {review.message}</p>
                                 <p>Date: {review.date}</p>
                                 <p>By: {users.find(user => user._id === review.user_id)?.username || 'User Not Found'}</p>
