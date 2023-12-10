82c7fc67a9a6f2489b87fdfdebb9723dd778f4e0
James <jamesflorez11@gmail.com>
2023-12-09 17:13:13 -0800

    Augment search results with current weather and associated channels

--- src/weather/results/index.js
+++ src/weather/results/index.js
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import {Link, useParams} from "react-router-dom";
 import * as client from "../client/client";
+import WeatherCard from "../card/weatherCard";
+import ChannelCard from "../card/channelCard";
 
 function Results() {
     const [locations, setLocations] = useState(null);
+    const [channels, setChannels] = useState([]);
     const {search} = useParams();
 
     const fetchLocations = async () => {
@@ -15,9 +18,18 @@ function Results() {
             console.error('Error fetching locations:', error);
         }
     };
+    const fetchChannels = async () => {
+        try {
+            const foundChannels = await client.getAllChannels();
+            setChannels(foundChannels);
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     useEffect(() => {
         fetchLocations();
+        fetchChannels();
     }, [search]);
 
     return (
@@ -30,11 +42,30 @@ function Results() {
                             .map((location, index) => (
                                 <Link to={`../details/${location.id}`}>
                                     <div key={index} className="card">
-                                        <h3 className="card-title">{location.name}</h3>
-                                        <p className="card-text">Region: {location.region}</p>
-                                        <p className="card-text">Country: {location.country}</p>
-                                        <p className="card-text">Latitude: {location.lat}</p>
-                                        <p className="card-text">Longitude: {location.lon}</p>
+                                        <div>
+                                            <h2 className="card-title">{location.name}</h2>
+                                            <p className="card-text">Region: {location.region}</p>
+                                            <p className="card-text">Country: {location.country}</p>
+                                            <p className="card-text">Latitude: {location.lat}</p>
+                                            <p className="card-text">Longitude: {location.lon}</p>
+                                        </div>
+                                        <div>
+                                            <h3>Current Weather</h3>
+                                            <WeatherCard id={location.id} />
+                                            <h3>Weather Channels for this Location</h3>
+                                            {channels && (
+                                                <div className="d-flex flex-wrap">
+                                                    {channels
+                                                        .filter((channel) => channel.location_id === location.id)
+                                                        .map((channel, channelIndex) => (
+                                                            <Link to={`../channels/${channel._id}`}>
+                                                                <ChannelCard channel={channel} key={channelIndex}/>
+                                                            </Link>
+                                                        ))}
+                                                </div>
+
+                                            )}
+                                        </div>
                                     </div>
                                 </Link>
                             ))
