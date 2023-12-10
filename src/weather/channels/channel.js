6feafc6506600b111b7972b10d5e99574f492d2e
James <jamesflorez11@gmail.com>
2023-12-09 23:22:24 -0800

    Start work for allowing forecasters to edit the channel page

--- src/weather/channels/channel.js
+++ src/weather/channels/channel.js
@@ -8,6 +8,7 @@ const Channel = () => {
     const [channel, setChannel] = useState(null);
     const [users, setUsers] = useState([]);
     const [reviews, setReviews] = useState([]);
+    const [account, setAccount] = useState(null);
     const { id } = useParams();
 
     const fetchChannelById = async () => {
@@ -34,11 +35,24 @@ const Channel = () => {
             console.log(err);
         }
     }
+    const fetchAccount = async () => {
+        try {
+            const foundAccount = await client.account();
+            setAccount(foundAccount);
+        } catch (err) {
+            if (err.response.status === 403) {
+                console.log("Not logged in");
+            } else {
+                console.log(err);
+            }
+        }
+    };
 
     useEffect(() => {
         fetchChannelById();
         fetchReviews();
         fetchUsers();
+        fetchAccount();
     }, [id]);
     
     return (
@@ -46,15 +60,50 @@ const Channel = () => {
             <h2>Channel Details</h2>
             <hr/>
             {channel ? (
-                <div className="card">
-                    <div className="card-body">
-                        <h3>{channel.name}</h3>
-                        <p>Description: {channel.description}</p>
-                        <p>City: {channel.city}</p>
-                        <p>Region: {channel.region}</p>
-                        <p>Country: {channel.country}</p>
+                (account && account.role === "FORECASTER") ? (
+                    <div>
+                        <h3>Channel Details Available to Edit</h3>
+                        <label htmlFor="channel_name">Channel Name</label>
+                        <input type="text"
+                               id="channel_name"
+                               value={channel.name}
+                               onChange={(e) => setChannel({...channel, name: e.target.value})}/>
+                        <br/>
+                        <label htmlFor="channel_description">Channel Description</label>
+                        <input type="text"
+                               id="channel_description"
+                               value={channel.description}
+                               onChange={(e) => setChannel({...channel, description: e.target.value})}/>
+                        <br/>
+                        <label htmlFor="channel_city">City</label>
+                        <input type="text"
+                               id="channel_city"
+                               value={channel.city}
+                               onChange={(e) => setChannel({...channel, city: e.target.value})}/>
+                        <br/>
+                        <label htmlFor="channel_region">Region</label>
+                        <input type="text"
+                               id="channel_region"
+                               value={channel.region}
+                               onChange={(e) => setChannel({...channel, region: e.target.value})}/>
+                        <br/>
+                        <label htmlFor="channel_country">Country</label>
+                        <input type="text"
+                               id="channel_country"
+                               value={channel.country}
+                               onChange={(e) => setChannel({...channel, country: e.target.value})}/>
                     </div>
-                </div>
+                ) : (
+                        <div className="card">
+                            <div className="card-body">
+                                <h3>{channel.name}</h3>
+                                <p>Description: {channel.description}</p>
+                                <p>City: {channel.city}</p>
+                                <p>Region: {channel.region}</p>
+                                <p>Country: {channel.country}</p>
+                            </div>
+                        </div>
+                )
             ) : (
                 <p>Loading Channel...</p>
             )}
