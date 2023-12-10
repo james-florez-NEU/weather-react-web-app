c5474f12f286efc74901b127c197dbcc00fe784b
James <jamesflorez11@gmail.com>
2023-12-09 16:25:49 -0800

    Add classnames to text in review card

--- src/weather/card/reviewCard.js
+++ src/weather/card/reviewCard.js
@@ -6,11 +6,11 @@ const ReviewCard = ({ review, channels, users }) => {
         <Link to = {`../reviews/${review._id}`}>
             <div className="card">
                 <div className="card-body">
-                    <p>Review of Weather Channel: {channels.find(channel => channel._id === review.channel_id)?.name || 'Channel Not Found'}</p>
-                    <p>Rating: {review.rating}/10</p>
-                    <p>Review: {review.message}</p>
-                    <p>Date: {review.date}</p>
-                    <p>By: {users.find(user => user._id === review.user_id)?.username || 'User Not Found'}</p>
+                    <h3 className="card-title">Review of Weather Channel: {channels.find(channel => channel._id === review.channel_id)?.name || 'Channel Not Found'}</h3>
+                    <p className="card-text">Rating: {review.rating}/10</p>
+                    <p className="card-text">Review: {review.message}</p>
+                    <p className="card-text">Date: {review.date}</p>
+                    <p className="card-text">By: {users.find(user => user._id === review.user_id)?.username || 'User Not Found'}</p>
                 </div>
             </div>
         </Link>
