b149029ac8dc5429a92f35ffd5ba1b2b8790d942
James <jamesflorez11@gmail.com>
2023-12-09 16:13:38 -0800

    Make ChannelCard a link

--- src/weather/card/channelCard.js
+++ src/weather/card/channelCard.js
@@ -1,14 +1,19 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
 const ChannelCard = ({ channel }) => {
     return (
-        <div className="card">
-            <div className="card-body">
-                <h3 className="card-title">{channel.name}</h3>
-                <p className="card-text">Description: {channel.description}</p>
-                <p className="card-text">City: {channel.city}</p>
-                <p className="card-text">Region: {channel.region}</p>
-                <p className="card-text">Country: {channel.country}</p>
+        <Link to = {`../channels/${channel._id}`}>
+            <div className="card">
+                <div className="card-body">
+                    <h3 className="card-title">{channel.name}</h3>
+                    <p className="card-text">Description: {channel.description}</p>
+                    <p className="card-text">City: {channel.city}</p>
+                    <p className="card-text">Region: {channel.region}</p>
+                    <p className="card-text">Country: {channel.country}</p>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 };
 export default ChannelCard;
